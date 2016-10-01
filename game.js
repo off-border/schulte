3e8d461b8588bab328ab8918506169660c477fde
off_border <off_border@bitbucket.org>
2016-10-01 17:27:34 +0200

    this.cells.slice() - > this.cells.splice()

--- game.js
+++ game.js
@@ -38,21 +38,12 @@ Shulte.prototype.start = function(){
 
 
 
+
 }
 
 Shulte.prototype.createCells = function(){
     snap.clear();
-    this.cells.slice(0,this.cells.length-1);
-
-    var tbWidth = this.snap.node.getBoundingClientRect().width - parseInt(getComputedStyle(this.snap.node)["border-left-width"]) * 2;
-
-    console.log('table width:', tbWidth);
-
-    var cellWidth = (tbWidth - this.cellPadding ) / this.size - this.cellPadding;
-
-    var fontSize = cellWidth / 2;
-
-
+    this.cells.splice(0,this.cells.length);
 
     var tmp = [];
     this.numbers = [];
@@ -66,35 +57,58 @@ Shulte.prototype.createCells = function(){
 
     for( let row = 0; row < this.size; row++ ){
         for( let col = 0; col < this.size; col++ ){
-            let x = ( cellWidth + this.cellPadding ) * col + this.cellPadding;
-            let y = ( cellWidth + this.cellPadding ) * row + this.cellPadding;
 
-            let cell = this.snap.rect(x,y,cellWidth,cellWidth);
-            cell.attr({
-                fill: this.cellBgColor,
-                stroke: this.cellBorderColor,
-                strokeWidth: this.cellBorderWidth,
-                rx: "1%",
-                ry: "1%"
-            });
-
-            cell.number = this.numbers[ this.size*row + col];
-            cell.text = snap.text( x,y, cell.number );
-            cell.text.attr({
-                x: x + cellWidth / 2,
-                y: y + cellWidth / 2 + fontSize/3 ,
-                fontSize: fontSize,
-                textAnchor: 'middle',
-                fill: this.cellFontColor,
-                cursor: 'default'
-
-            });
+            let cell = this.snap.rect(0,0,0,0);
+            cell.number = this.numbers[ this.size*row + col ];
+            cell.text = snap.text( 0,0, cell.number );
 
             cell.mousedown( this.cellClicked.bind(this,cell) );
             cell.text.mousedown( this.cellClicked.bind(this,cell) );
 
             this.cells.push(cell);
 
+        }
+    }
+
+    this.alignCells();
+
+}
+
+Shulte.prototype.alignCells = function(){
+
+    var tbWidth = this.snap.node.getBoundingClientRect().width - parseInt(getComputedStyle(this.snap.node)["border-left-width"]) * 2;
+    var cellWidth = (tbWidth - this.cellPadding ) / this.size - this.cellPadding;
+    var fontSize = cellWidth / 2;
+
+    for( let row = 0; row < this.size; row++ ){
+        for( let col = 0; col < this.size; col++ ){
+
+            let x = ( cellWidth + this.cellPadding ) * col + this.cellPadding;
+            let y = ( cellWidth + this.cellPadding ) * row + this.cellPadding;
+
+            let cell = this.cells[ this.size*row + col ];
+
+            console.log('align cell:', cell.number );
+
+            cell.attr({
+                x: x,
+                y: y,
+                width: cellWidth,
+                height: cellWidth,
+                fill: this.cellBgColor,
+                stroke: this.cellBorderColor,
+                strokeWidth: this.cellBorderWidth,
+                rx: "1%",
+                ry: "1%"
+            });
+            cell.text.attr({
+                x: x + cellWidth / 2,
+                y: y + cellWidth / 2 + fontSize/3 ,
+                fontSize: fontSize,
+                textAnchor: 'middle',
+                fill: this.cellFontColor,
+                cursor: 'default',
+            });
         }
     }
 
